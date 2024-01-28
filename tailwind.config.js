badd9ae575d3056fe8486f6fe8ab17c4d8944914
Farhan <faarhan7@gmail.com>
2024-01-28 17:34:02 +0700

    fix conflict component name

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./vendor/wireui/wireui/src/*.php",
+        "./vendor/wireui/wireui/ts/**/*.ts",
+        "./vendor/wireui/wireui/src/View/**/*.php",
+        "./vendor/wireui/wireui/src/WireUi/**/*.php",
+        "./vendor/wireui/wireui/src/resources/**/*.blade.php",
     ],
 
     theme: {
@@ -16,6 +21,10 @@ export default {
             },
         },
     },
+
+    presets: [
+        require("./vendor/wireui/wireui/tailwind.config.js")
+    ],
 
     plugins: [forms],
 };
