302c798e1069f0492566f715696670feae2137b3
Farhan <faarhan7@gmail.com>
2024-01-29 11:36:49 +0700

    adjust margin

--- resources/views/temdashboard.blade.php
+++ resources/views/temdashboard.blade.php
@@ -165,7 +165,7 @@
         </div>
     </aside>
 
-    <div class="p-4 transition-all sm:ml-64" :class="{ 'sm:ml-36': !sidebarOpen }">
+    <div class="p-4 transition-all sm:ml-64" :class="{ 'sm:ml-28': !sidebarOpen }">
         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
             <div class="grid grid-cols-3 gap-4 mb-4">
                 <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
