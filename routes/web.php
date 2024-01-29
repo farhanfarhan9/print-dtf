fd42980c2be9872dfd14f73a577d8ac4c94f4e61
Farhan <faarhan7@gmail.com>
2024-01-29 08:14:41 +0700

    template dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
+Route::view('/tem', 'temdashboard');
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
