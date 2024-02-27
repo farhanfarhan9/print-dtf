26beb9bb2e62a3ac50d2b5d3c636cc992c3fd5ca
Bayu Apriansah <bayu@sustainablelivinglab.org>
2024-02-27 09:36:46 +0700

    styling product

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -116,6 +116,26 @@
                         <span class="ms-3" :class="{ 'hidden': !sidebarOpen }">Informasi Bank</span>
                     </a>
                 </li>
+                <li>
+                    <a href="{{route('products-view')}}"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                        :class="{ 'justify-center': !sidebarOpen }">
+                        <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+                          </svg>
+                        <span class="ms-3" :class="{ 'hidden': !sidebarOpen }">Produk</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('ekspedisi-view')}}"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                        :class="{ 'justify-center': !sidebarOpen }">
+                        <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+                          </svg>
+                        <span class="ms-3" :class="{ 'hidden': !sidebarOpen }">Ekspedisi</span>
+                    </a>
+                </li>
             </div>
         </ul>
     </div>
--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -1,56 +1,72 @@
 <div>
-    <div class="my-4">
-        <x-button label="+ Tambah Data Produk" primary wire:click='addData' />
-    </div>
-
-    <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr>
-                    <th scope="col" class="py-3 px-6">Product Name</th>
-                    <th scope="col" class="py-3 px-6">Stock</th>
-                    <th scope="col" class="py-3 px-6">Price Details</th>
-                    <th scope="col" class="py-3 px-6">Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if($products->first() == null)
-                <tr class="bg-white border-b">
-                    <td class="py-4 px-6 text-center" colspan="3">Data Kosong</td>
-                </tr>
-                @endif
-                @foreach($products as $product)
-                    <tr class="bg-white border-b">
-                        <td class="py-4 px-6">{{ $product->nama_produk }}</td>
-                        <td class="py-4 px-6">{{ $product->stok }}</td>
-                        <td class="py-4 px-6">
-                        @php
-                        $detailHarga = json_decode($product->detail_harga, true);
-                        @endphp
-
-                        @foreach($detailHarga as $detail)
-                            Range: {{ $detail['start'] }} m - {{ $detail['end'] }} m, Harga: Rp.{{ $detail['price'] }}<br>
-                        @endforeach
-                        </td>
-                        <td class="py-4 px-6">
-                            <div class="flex items-center space-x-4">
-                                <!-- Edit Button -->
-                                <button type="button" class="p-2 text-sm text-blue-600 bg-blue-100 rounded-full hover:bg-blue-200 focus:outline-none focus:ring" wire:click="editProduct({{ $product->id }})">
-                                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17h2v2h-2v-2z m0 0v-6h2v6m-2 0h-3.5a2 2 0 0 1-2-2v-3.5a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3.5a2 2 0 0 1-2 2H11z"></path></svg>
-                                </button>
-
-                                <!-- Delete Button -->
-                                <button type="button" class="p-2 text-sm text-red-600 bg-red-100 rounded-full hover:bg-red-200 focus:outline-none focus:ring" x-on:click="$openModal('deleteModal')" wire:click="confirmDelete({{ $product->id }})">
-                                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m8-4H7a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2z"></path></svg>
-                                </button>
-                            </div>
-                        </td>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="text-3xl font-semibold leading-tight text-gray-800">
+                Produk
+            </h2>
+        </div>
+    </x-slot>
+    <div class="py-12">
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+        <div class="flex sm:justify-between">
+            <x-input wire:model.live.debounce.300ms="search" icon="search" class="sm:!w-1/4" shadowless="true"
+                placeholder="Cari Produk" />
+            <x-button label="Tambah Data Produk" href="{{ route('product.add') }}" wire:navigate class="w-1/3 mt-2 sm:w-1/6 sm:mt-0" green
+                icon="plus" />
+        </div>
+        {{-- <x-button label="+ Tambah Data Produk" primary wire:click='addData' /> --}}
+        <div class="overflow-x-auto mt-5 relative shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                        <th scope="col" class="py-3 px-6">Product Name</th>
+                        <th scope="col" class="py-3 px-6">Stock</th>
+                        <th scope="col" class="py-3 px-6">Price Details</th>
+                        <th scope="col" class="py-3 px-6">Aksi</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+                </thead>
+                <tbody>
+                    @if($products->first() == null)
+                    <tr class="bg-white border-b">
+                        <td class="py-4 px-6 text-center" colspan="3">Data Kosong</td>
+                    </tr>
+                    @endif
+                    @foreach($products as $product)
+                        <tr class="bg-white border-b">
+                            <td class="py-4 px-6">{{ $product->nama_produk }}</td>
+                            <td class="py-4 px-6">{{ $product->stok }}</td>
+                            <td class="py-4 px-6">
+                            @php
+                            $detailHarga = json_decode($product->detail_harga, true);
+                            @endphp
 
+                            @foreach($detailHarga as $detail)
+                                Range: {{ $detail['start'] }} m - {{ $detail['end'] }} m, Harga: Rp.{{ $detail['price'] }}<br>
+                            @endforeach
+                            </td>
+                            <td class="py-4 px-6">
+                                <div class="flex items-center space-x-4">
+                                    <!-- Edit Button -->
+                                    <button type="button" class="p-2 text-sm text-blue-600 bg-blue-100 rounded-full hover:bg-blue-200 focus:outline-none focus:ring" wire:click="editProduct({{ $product->id }})">
+                                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17h2v2h-2v-2z m0 0v-6h2v6m-2 0h-3.5a2 2 0 0 1-2-2v-3.5a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3.5a2 2 0 0 1-2 2H11z"></path></svg>
+                                    </button>
+
+                                    <!-- Delete Button -->
+                                    <button type="button" class="p-2 text-sm text-red-600 bg-red-100 rounded-full hover:bg-red-200 focus:outline-none focus:ring" x-on:click="$openModal('deleteModal')" wire:click="confirmDelete({{ $product->id }})">
+                                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m8-4H7a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2z"></path></svg>
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
     <x-modal name="deleteModal">
         <x-card>
             <x-slot name="title">Delete Product</x-slot>
@@ -65,3 +81,4 @@
         </x-card>
     </x-modal>
 </div>
+
