aec129366a6730f2dc2e8521542c6ea0930ee08b
Bayu Apriansah <bayu@sustainablelivinglab.org>
2024-02-27 09:42:45 +0700

    styling ekspedisi - product

--- resources/views/livewire/ekspedisi/ekspedisis.blade.php
+++ resources/views/livewire/ekspedisi/ekspedisis.blade.php
@@ -40,15 +40,12 @@
                             <td class="py-4 px-6">{{ $ekspedisi->ongkir }}</td>
                             <td class="py-4 px-6">
                                 <div class="flex items-center space-x-4">
-                                    <!-- Edit Button -->
-                                    <button type="button" class="p-2 text-sm text-blue-600 bg-blue-100 rounded-full hover:bg-blue-200 focus:outline-none focus:ring" wire:click="editEkspedisi({{ $ekspedisi->id }})">
-                                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 17h2v2h-2v-2z m0 0v-6h2v6m-2 0h-3.5a2 2 0 0 1-2-2v-3.5a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3.5a2 2 0 0 1-2 2H11z"></path></svg>
-                                    </button>
-
-                                    <!-- Delete Button -->
-                                    <button type="button" class="p-2 text-sm text-red-600 bg-red-100 rounded-full hover:bg-red-200 focus:outline-none focus:ring" x-on:click="$openModal('deleteModal')" wire:click="confirmDelete({{ $ekspedisi->id }})">
-                                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m8-4H7a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2z"></path></svg>
-                                    </button>
+                                    <div class="flex gap-5">
+                                        <x-button wire:click="editEkspedisi({{ $ekspedisi->id }})" label="Edit"
+                                            primary />
+                                        <x-button wire:click="delete({{ $ekspedisi->id }})"
+                                            wire:confirm="Apakah Anda yakin menghapus data ini? ?" label="Hapus" red />
+                                    </div>
                                 </div>
                             </td>
                         </tr>
@@ -56,19 +53,5 @@
                 </tbody>
             </table>
         </div>
-
-        <x-modal name="deleteModal">
-            <x-card>
-                <x-slot name="title">Delete Ekspedisi</x-slot>
-                <p>
-                    Are you sure you want to delete this Ekspedisi?
-                </p>
-                <x-slot name="footer">
-                    <x-button flat label="Cancel" wire:click="$set('confirmingEkspedisiDeletion', null)" x-on:click="close" />
-
-                    <x-button primary label="I Agree" wire:click="deleteEkspedisi" x-on:click="close"/>
-                </x-slot>
-            </x-card>
-        </x-modal>
     </div>
 </div>
