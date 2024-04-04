0e8cd09d68054a66a14f1901d5a9a6835679dc11
Bayu Apriansah <bayu@sustainablelivinglab.org>
2024-04-04 09:22:24 +0700

    fix : numbering order in table when import the user data from excel fix start from 1 not from 4

--- resources/views/livewire/customer-import-data.blade.php
+++ resources/views/livewire/customer-import-data.blade.php
@@ -46,7 +46,7 @@
                             class="border-b odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700">
                             <th scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{ $key }}
+                                {{ $key-3 }}
                             </th>
                             <td class="px-6 py-4">
                                 {{ $value[1] }}
