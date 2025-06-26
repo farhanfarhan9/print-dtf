01420d8a8c99096fc16f806a3ac78988580f4caf
Farhan <faarhan7@gmail.com>
2025-06-27 04:50:14 +0700

    feat: add owner role

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,42 @@
 <?php
 
 use App\Livewire\Product;
+use App\Livewire\Dashboard;
 use App\Livewire\ProductAdd;
 use App\Livewire\ProductEdit;
 use App\Livewire\Bank\AllBank;
+use App\Livewire\User\AllUser;
+use App\Livewire\User\EditUser;
 use App\Livewire\Order\AllOrder;
 use App\Livewire\Order\Po\AllPo;
 use App\Livewire\Order\Po\EditPo;
+use App\Livewire\User\CreateUser;
 use App\Livewire\Order\CreateOrder;
+use App\Livewire\User\ArchieveUser;
 use App\Livewire\Address\AllAddress;
+use App\Livewire\CustomerImportData;
 use App\Livewire\Address\EditAddress;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Customer\AllCustomer;
-use App\Livewire\CustomerImportData;
 use App\Livewire\Ekspedisi\Ekspedisis;
 use App\Livewire\Address\CreateAddress;
 use App\Livewire\Customer\EditCustomer;
 use App\Livewire\Ekspedisi\EkspedisiAdd;
 use App\Http\Controllers\OrderController;
-use App\Http\Middleware\isAdminMiddleware;
 use App\Livewire\Customer\CreateCustomer;
 use App\Livewire\Ekspedisi\EkspedisiEdit;
-use App\Livewire\Bank\CreateBankInformations;
+use App\Http\Middleware\isAdminMiddleware;
+use App\Http\Middleware\isOwnerMiddleware;
 use App\Livewire\Customer\ArchieveCustomer;
-use App\Livewire\Dashboard;
+use App\Livewire\Bank\CreateBankInformations;
 use App\Livewire\Ekspedisi\EkspedisiArchieve;
-use App\Livewire\InternalProcess\AllInternalProcess;
-use App\Livewire\ExportData\ExportCustomerView;
 use App\Livewire\ExportData\ExportProductView;
+use App\Livewire\ExportData\ExportCustomerView;
+use App\Http\Middleware\isAdminOrOwnerMiddleware;
 use App\Livewire\ExportData\ExportBookkeepingView;
-use App\Livewire\InternalProcess\AllInternalProcessWithoutMachine;
+use App\Livewire\InternalProcess\AllInternalProcess;
 use App\Livewire\InternalProcess\HistoryInternalProcess;
-use App\Livewire\User\AllUser;
-use App\Livewire\User\ArchieveUser;
-use App\Livewire\User\CreateUser;
-use App\Livewire\User\EditUser;
+use App\Livewire\InternalProcess\AllInternalProcessWithoutMachine;
 
 /*
 |--------------------------------------------------------------------------
@@ -69,17 +71,17 @@ Route::middleware(['auth'])->group(function () {
     });
 
     Route::prefix('customers')->group(function () {
-        Route::get('/', AllCustomer::class)->name('customer.index')->middleware(isAdminMiddleware::class);
-        Route::get('/create', CreateCustomer::class)->name('customer.create')->middleware(isAdminMiddleware::class);
-        Route::get('/upload', CustomerImportData::class)->name('customer.upload')->middleware(isAdminMiddleware::class);
-        Route::get('/{customer}/edit', EditCustomer::class)->name('customer.edit')->middleware(isAdminMiddleware::class);
-        Route::get('/archieve', ArchieveCustomer::class)->name('customer.archieve')->middleware(isAdminMiddleware::class);
+        Route::get('/', AllCustomer::class)->name('customer.index')->middleware([isAdminOrOwnerMiddleware::class]);
+        Route::get('/create', CreateCustomer::class)->name('customer.create')->middleware([isAdminOrOwnerMiddleware::class]);
+        Route::get('/upload', CustomerImportData::class)->name('customer.upload')->middleware([isAdminOrOwnerMiddleware::class]);
+        Route::get('/{customer}/edit', EditCustomer::class)->name('customer.edit')->middleware([isAdminOrOwnerMiddleware::class]);
+        Route::get('/archieve', ArchieveCustomer::class)->name('customer.archieve')->middleware([isAdminOrOwnerMiddleware::class]);
     });
     Route::prefix('users')->group(function () {
-        Route::get('/', AllUser::class)->name('user.index')->middleware(isAdminMiddleware::class);
-        Route::get('/create', CreateUser::class)->name('user.create')->middleware(isAdminMiddleware::class);
-        Route::get('/{user}/edit', EditUser::class)->name('user.edit')->middleware(isAdminMiddleware::class);
-        Route::get('/archieve', ArchieveUser::class)->name('user.archieve')->middleware(isAdminMiddleware::class);
+        Route::get('/', AllUser::class)->name('user.index')->middleware(isOwnerMiddleware::class);
+        Route::get('/create', CreateUser::class)->name('user.create')->middleware(isOwnerMiddleware::class);
+        Route::get('/{user}/edit', EditUser::class)->name('user.edit')->middleware(isOwnerMiddleware::class);
+        Route::get('/archieve', ArchieveUser::class)->name('user.archieve')->middleware(isOwnerMiddleware::class);
     });
 
     Route::prefix('bank-informations')->group(function () {
@@ -88,27 +90,27 @@ Route::middleware(['auth'])->group(function () {
         // Route::get('/{customer}/edit', EditCustomer::class)->name('customer.edit');
     });
 
-    Route::get('/products', Product::class)->name('products-view')->middleware(isAdminMiddleware::class);
-    Route::get('/products/add', ProductAdd::class)->name('product.add')->middleware(isAdminMiddleware::class);
-    Route::get('/product/edit/{product}', ProductEdit::class)->name('product-edit')->middleware(isAdminMiddleware::class);
+    Route::get('/products', Product::class)->name('products-view')->middleware(isOwnerMiddleware::class);
+    Route::get('/products/add', ProductAdd::class)->name('product.add')->middleware(isOwnerMiddleware::class);
+    Route::get('/product/edit/{product}', ProductEdit::class)->name('product-edit')->middleware(isOwnerMiddleware::class);
 
-    Route::get('/ekspedisi', Ekspedisis::class)->name('ekspedisi-view')->middleware(isAdminMiddleware::class);
-    Route::get('/ekspedisi/add', EkspedisiAdd::class)->name('ekspedisi.add')->middleware(isAdminMiddleware::class);
-    Route::get('/ekspedisi/edit/{ekspedisi}', EkspedisiEdit::class)->name('ekspedisi-edit')->middleware(isAdminMiddleware::class);
-    Route::get('/ekspedisi/archieve', EkspedisiArchieve::class)->name('ekspedisi-archieve')->middleware(isAdminMiddleware::class);
+    Route::get('/ekspedisi', Ekspedisis::class)->name('ekspedisi-view')->middleware(isOwnerMiddleware::class);
+    Route::get('/ekspedisi/add', EkspedisiAdd::class)->name('ekspedisi.add')->middleware(isOwnerMiddleware::class);
+    Route::get('/ekspedisi/edit/{ekspedisi}', EkspedisiEdit::class)->name('ekspedisi-edit')->middleware(isOwnerMiddleware::class);
+    Route::get('/ekspedisi/archieve', EkspedisiArchieve::class)->name('ekspedisi-archieve')->middleware(isOwnerMiddleware::class);
 
     Route::prefix('orders')->group(function () {
-        Route::get('/', AllOrder::class)->name('order.index')->middleware(isAdminMiddleware::class);
-        Route::get('/create', CreateOrder::class)->name('order.create')->middleware(isAdminMiddleware::class);
+        Route::get('/', AllOrder::class)->name('order.index')->middleware(isAdminOrOwnerMiddleware::class);
+        Route::get('/create', CreateOrder::class)->name('order.create')->middleware(isAdminOrOwnerMiddleware::class);
 
-        Route::get('/{order}/purchase_order', AllPo::class)->name('po.allPo')->middleware(isAdminMiddleware::class);
-        Route::get('/{order}/purchase_order/{po}/edit', EditPo::class)->name('po.editPo')->middleware(isAdminMiddleware::class);
+        Route::get('/{order}/purchase_order', AllPo::class)->name('po.allPo')->middleware(isAdminOrOwnerMiddleware::class);
+        Route::get('/{order}/purchase_order/{po}/edit', EditPo::class)->name('po.editPo')->middleware(isAdminOrOwnerMiddleware::class);
     });
 
     Route::prefix('export-data')->group(function () {
-        Route::get('/customer', ExportCustomerView::class)->name('export-customer.index');
-        Route::get('/product', ExportProductView::class)->name('export-product.index');
-        Route::get('/bookkeeping/{type?}', ExportBookkeepingView::class)->name('export-bookkeeping.index');
+        Route::get('/customer', ExportCustomerView::class)->name('export-customer.index')->middleware(isOwnerMiddleware::class);
+        Route::get('/product', ExportProductView::class)->name('export-product.index')->middleware(isAdminOrOwnerMiddleware::class);
+        Route::get('/bookkeeping/{type?}', ExportBookkeepingView::class)->name('export-bookkeeping.index')->middleware(isAdminOrOwnerMiddleware::class);
     });
 
     Route::prefix('internal_process')->group(function () {
